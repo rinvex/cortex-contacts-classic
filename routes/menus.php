d101da8e5ecf074a83fcaf84766eec6d54429e36
Abdelrahman Omran <me@omranic.com>
2017-08-12 17:34:20 +0200

    Fix code style

--- routes/menus.php
+++ routes/menus.php
@@ -1,4 +1,5 @@
 <?php
 
 declare(strict_types=1);
+
 Menu::backendSidebar('resources')->routeIfCan('list-contacts', 'backend.contacts.index', '<i class="fa fa-briefcase"></i> <span>'.trans('cortex/contacts::common.contacts').'</span>');
