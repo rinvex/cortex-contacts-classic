09792e233ffcad281766ed3f49778aaa7e15458d
Abdelrahman Omran <me@omranic.com>
2017-08-16 19:01:28 +0200

    Enforce consistency

--- src/Providers/ContactsServiceProvider.php
+++ src/Providers/ContactsServiceProvider.php
@@ -10,6 +10,20 @@ use Illuminate\Support\ServiceProvider;
 class ContactsServiceProvider extends ServiceProvider
 {
     /**
+     * Register any application services.
+     *
+     * This service provider is a great spot to register your various container
+     * bindings with the application. As you can see, we are registering our
+     * "Registrar" implementation here. You can add your own bindings too!
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
      * Bootstrap any application services.
      *
      * @return void
@@ -24,20 +38,6 @@ class ContactsServiceProvider extends ServiceProvider
 
         // Publish Resources
         ! $this->app->runningInConsole() || $this->publishResources();
-    }
-
-    /**
-     * Register any application services.
-     *
-     * This service provider is a great spot to register your various container
-     * bindings with the application. As you can see, we are registering our
-     * "Registrar" implementation here. You can add your own bindings too!
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
     }
 
     /**
