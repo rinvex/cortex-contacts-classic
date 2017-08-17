2264caf22cf0fd72eb92876a0d2a1452aa3f54dc
Abdelrahman Omran <me@omranic.com>
2017-08-17 15:14:30 +0200

    Drop cortex/fort dependency

--- src/Policies/ContactPolicy.php
+++ src/Policies/ContactPolicy.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Cortex\Contacts\Policies;
 
-use Cortex\Fort\Models\User;
+use Rinvex\Fort\Models\User;
 use Cortex\Contacts\Models\Contact;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
@@ -16,7 +16,7 @@ class ContactPolicy
      * Determine whether the user can list contacts.
      *
      * @param string                   $ability
-     * @param \Cortex\Fort\Models\User $user
+     * @param \Rinvex\Fort\Models\User $user
      *
      * @return bool
      */
@@ -29,7 +29,7 @@ class ContactPolicy
      * Determine whether the user can create contacts.
      *
      * @param string                   $ability
-     * @param \Cortex\Fort\Models\User $user
+     * @param \Rinvex\Fort\Models\User $user
      *
      * @return bool
      */
@@ -42,7 +42,7 @@ class ContactPolicy
      * Determine whether the user can update the contact.
      *
      * @param string                          $ability
-     * @param \Cortex\Fort\Models\User        $user
+     * @param \Rinvex\Fort\Models\User        $user
      * @param \Cortex\Contacts\Models\Contact $resource
      *
      * @return bool
@@ -56,7 +56,7 @@ class ContactPolicy
      * Determine whether the user can delete the contact.
      *
      * @param string                          $ability
-     * @param \Cortex\Fort\Models\User        $user
+     * @param \Rinvex\Fort\Models\User        $user
      * @param \Cortex\Contacts\Models\Contact $resource
      *
      * @return bool
