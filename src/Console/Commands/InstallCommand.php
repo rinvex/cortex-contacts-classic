f87ba86830e2b8fb28db8cdaef46811af734b429
Abdelrahman Omran <me@omranic.com>
2017-10-29 05:25:05 +0200

    Tweak artisan command output lines

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -29,7 +29,7 @@ class InstallCommand extends Command
      */
     public function handle()
     {
-        $this->warn('Install cortex/contacts:');
+        $this->warn($this->description);
         $this->call('cortex:migrate:contacts');
         $this->call('cortex:seed:contacts');
         $this->call('cortex:publish:contacts');
