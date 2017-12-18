c66a71b89e72ca83962e73d0c79b320f73e792b2
Abdelrahman Omran <me@omranic.com>
2017-12-18 18:47:16 +0200

    Refactor menus

--- routes/menus.php
+++ routes/menus.php
@@ -2,5 +2,17 @@
 
 declare(strict_types=1);
 
-Menu::adminareaSidebar('resources')->routeIfCan('list-contacts', 'adminarea.contacts.index', '<i class="fa fa-briefcase"></i> <span>'.trans('cortex/contacts::common.contacts').'</span>');
-Menu::managerareaSidebar('resources')->routeIfCan('list-contacts', 'managerarea.contacts.index', '<i class="fa fa-briefcase"></i> <span>'.trans('cortex/contacts::common.contacts').'</span>');
+use Rinvex\Menus\Models\MenuItem;
+use Rinvex\Menus\Factories\MenuFactory;
+
+Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
+    $menu->findBy('title', trans('cortex/foundation::common.crm'), function (MenuItem $dropdown) {
+        $dropdown->route(['adminarea.contacts.index'], trans('cortex/contacts::common.contacts'), 10, 'fa fa-id-card-o')->can('list-contacts');
+    });
+});
+
+Menu::modify('managerarea.sidebar', function(MenuFactory $menu) {
+    $menu->findBy('title', trans('cortex/foundation::common.crm'), function (MenuItem $dropdown) {
+        $dropdown->route(['managerarea.contacts.index'], trans('cortex/contacts::common.contacts'), 10, 'fa fa-id-card-o')->can('list-contacts');
+    });
+});
