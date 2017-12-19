94446f71072340a01473413f97edf3a4ce985ef1
Abdelrahman Omran <me@omranic.com>
2017-12-19 15:38:14 +0000

    Apply fixes from StyleCI

--- routes/menus.php
+++ routes/menus.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 use Rinvex\Menus\Models\MenuItem;
 use Rinvex\Menus\Factories\MenuFactory;
 
-Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
+Menu::modify('adminarea.sidebar', function (MenuFactory $menu) {
     $menu->findByTitleOrAdd(trans('cortex/foundation::common.crm'), 50, 'fa fa-briefcase', [], function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.contacts.index'], trans('cortex/contacts::common.contacts'), 10, 'fa fa-id-card-o')->can('list-contacts');
     });
 });
 
-Menu::modify('managerarea.sidebar', function(MenuFactory $menu) {
+Menu::modify('managerarea.sidebar', function (MenuFactory $menu) {
     $menu->findByTitleOrAdd(trans('cortex/foundation::common.crm'), 50, 'fa fa-briefcase', [], function (MenuItem $dropdown) {
         $dropdown->route(['managerarea.contacts.index'], trans('cortex/contacts::common.contacts'), 10, 'fa fa-id-card-o')->can('list-contacts');
     });
