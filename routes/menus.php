76ca593ba7365f9bed699cc6f505d16c6a5159a8
Abdelrahman Omran <me@omranic.com>
2017-12-19 17:38:02 +0200

    Tweak menus

--- routes/menus.php
+++ routes/menus.php
@@ -6,13 +6,13 @@ use Rinvex\Menus\Models\MenuItem;
 use Rinvex\Menus\Factories\MenuFactory;
 
 Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
-    $menu->findBy('title', trans('cortex/foundation::common.crm'), function (MenuItem $dropdown) {
+    $menu->findByTitleOrAdd(trans('cortex/foundation::common.crm'), 50, 'fa fa-briefcase', [], function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.contacts.index'], trans('cortex/contacts::common.contacts'), 10, 'fa fa-id-card-o')->can('list-contacts');
     });
 });
 
 Menu::modify('managerarea.sidebar', function(MenuFactory $menu) {
-    $menu->findBy('title', trans('cortex/foundation::common.crm'), function (MenuItem $dropdown) {
+    $menu->findByTitleOrAdd(trans('cortex/foundation::common.crm'), 50, 'fa fa-briefcase', [], function (MenuItem $dropdown) {
         $dropdown->route(['managerarea.contacts.index'], trans('cortex/contacts::common.contacts'), 10, 'fa fa-id-card-o')->can('list-contacts');
     });
 });
