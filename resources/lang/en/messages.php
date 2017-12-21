70b9cbca2dc0b1895ef0ab0987551c1032ab1bb4
Abdelrahman Omran <me@omranic.com>
2017-12-21 21:11:07 +0200

    Remove useless language phrase

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -7,7 +7,6 @@ return [
     'contact' => [
         'saved' => 'Congrats! Contact [:slug] saved successfully.',
         'deleted' => 'Done! Contact [:slug] deleted successfully.',
-        'not_found' => 'Sorry! Contact [:contactSlug] not found.',
     ],
 
 ];
