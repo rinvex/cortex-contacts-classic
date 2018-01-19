0b7f85a2b771e7e677d8cd64f1fd4f9adf5858c4
Abdelrahman Omran <me@omranic.com>
2018-01-19 02:26:37 +0200

    Remove useless loading migrations command

--- src/Providers/ContactsServiceProvider.php
+++ src/Providers/ContactsServiceProvider.php
@@ -65,7 +65,6 @@ class ContactsServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'cortex/contacts');
         $this->loadTranslationsFrom(__DIR__.'/../../resources/lang', 'cortex/contacts');
-        ! $this->app->runningInConsole() || $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
         $this->app->afterResolving('blade.compiler', function () {
             require __DIR__.'/../../routes/menus.php';
         });
