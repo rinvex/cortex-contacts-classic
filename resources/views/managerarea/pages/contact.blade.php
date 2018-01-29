58579f56d672cbac7ff6fe9dbd8c688deda179c5
Abdelrahman Omran <me@omranic.com>
2018-01-29 20:25:16 +0200

    Tweak deletion button

--- resources/views/managerarea/pages/contact.blade.php
+++ resources/views/managerarea/pages/contact.blade.php
@@ -31,10 +31,10 @@
         <section class="content">
 
             <div class="nav-tabs-custom">
+                @if($contact->exists && $currentUser->can('delete-contacts', $contact)) <div class="pull-right"><a href="#" data-toggle="modal" data-target="#delete-confirmation" data-modal-action="{{ route('managerarea.contacts.delete', ['contact' => $contact]) }}" data-modal-title="{!! trans('cortex/foundation::messages.delete_confirmation_title') !!}" data-modal-body="{!! trans('cortex/foundation::messages.delete_confirmation_body', ['type' => 'contact', 'name' => $contact->name]) !!}" title="{{ trans('cortex/foundation::common.delete') }}" class="btn btn-default" style="margin: 4px"><i class="fa fa-trash text-danger"></i></a></div> @endif
                 <ul class="nav nav-tabs">
                     <li class="active"><a href="#details-tab" data-toggle="tab">{{ trans('cortex/contacts::common.details') }}</a></li>
                     @if($contact->exists) <li><a href="#logs-tab" data-toggle="tab">{{ trans('cortex/contacts::common.logs') }}</a></li> @endif
-                    @if($contact->exists && $currentUser->can('delete-contacts', $contact)) <li class="pull-right"><a href="#" data-toggle="modal" data-target="#delete-confirmation" data-modal-action="{{ route('managerarea.contacts.delete', ['contact' => $contact]) }}" data-modal-title="{!! trans('cortex/foundation::messages.delete_confirmation_title') !!}" data-modal-body="{!! trans('cortex/foundation::messages.delete_confirmation_body', ['type' => 'contact', 'name' => $contact->name]) !!}" title="{{ trans('cortex/foundation::common.delete') }}"><i class="fa fa-trash text-danger"></i></a></li> @endif
                 </ul>
 
                 <div class="tab-content">
