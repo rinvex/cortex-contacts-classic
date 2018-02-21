1422de149da40d0aa9cf0670632f0afa0c725796
Abdelrahman Omran <me@omranic.com>
2018-02-21 06:57:19 +0200

    Grant owner role abilities

--- database/seeds/CortexContactsSeeder.php
+++ database/seeds/CortexContactsSeeder.php
@@ -18,5 +18,11 @@ class CortexContactsSeeder extends Seeder
         Bouncer::allow('admin')->to('update', config('rinvex.contacts.models.contact'));
         Bouncer::allow('admin')->to('delete', config('rinvex.contacts.models.contact'));
         Bouncer::allow('admin')->to('audit', config('rinvex.contacts.models.contact'));
+
+        Bouncer::allow('owner')->to('list', config('rinvex.contacts.models.contact'));
+        Bouncer::allow('owner')->to('create', config('rinvex.contacts.models.contact'));
+        Bouncer::allow('owner')->to('update', config('rinvex.contacts.models.contact'));
+        Bouncer::allow('owner')->to('delete', config('rinvex.contacts.models.contact'));
+        Bouncer::allow('owner')->to('audit', config('rinvex.contacts.models.contact'));
     }
 }
