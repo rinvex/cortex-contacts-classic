c4a48a1628dd65ad6bf7aab8d8b9a69dcd54ce09
Abdelrahman Omran <me@omranic.com>
2018-05-28 21:42:53 +0200

    Fix social attributes issues

---
+++ database/migrations/2018_05_28_163915_alter_contacts_table_add_social_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterContactsTableAddSocialColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(config('rinvex.contacts.tables.contacts'), function (Blueprint $table) {
+            $table->schemalessAttributes('social')->after('gender');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(config('rinvex.contacts.tables.contacts'), function (Blueprint $table) {
+            $table->dropColumn('social');
+        });
+    }
+}
