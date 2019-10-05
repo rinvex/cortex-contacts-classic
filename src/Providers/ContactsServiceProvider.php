33ed19aebf87d8f978a027ef174eeed1661da037
Abdelrahman Omran <me@omranic.com>
2019-10-05 23:51:22 +0400

    Refactor menus and breadcrumb bindings to utilize event dispatcher

--- src/Providers/ContactsServiceProvider.php
+++ src/Providers/ContactsServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Routing\Router;
 use Cortex\Contacts\Models\Contact;
 use Illuminate\Support\ServiceProvider;
 use Rinvex\Support\Traits\ConsoleTools;
+use Illuminate\Contracts\Events\Dispatcher;
 use Cortex\Contacts\Console\Commands\SeedCommand;
 use Cortex\Contacts\Console\Commands\InstallCommand;
 use Cortex\Contacts\Console\Commands\MigrateCommand;
@@ -56,7 +57,7 @@ class ContactsServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(Router $router): void
+    public function boot(Router $router, Dispatcher $dispatcher): void
     {
         // Bind route models and constrains
         $router->pattern('contact', '[a-zA-Z0-9-]+');
@@ -72,8 +73,8 @@ class ContactsServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../../routes/web/managerarea.php');
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'cortex/contacts');
         $this->loadTranslationsFrom(__DIR__.'/../../resources/lang', 'cortex/contacts');
-        $this->app->runningInConsole() || $this->app->afterResolving('blade.compiler', function () {
-            $accessarea = $this->app['request']->route('accessarea');
+
+        $this->app->runningInConsole() || $dispatcher->listen('controller.constructed', function ($accessarea) {
             ! file_exists($menus = __DIR__."/../../routes/menus/{$accessarea}.php") || require $menus;
             ! file_exists($breadcrumbs = __DIR__."/../../routes/breadcrumbs/{$accessarea}.php") || require $breadcrumbs;
         });
