63ddc8f45c88c0e5c2ad0e1f9ff71f1ffeee0c0e
Abdelrahman Omran <me@omranic.com>
2020-05-26 05:16:22 +0400

    Remove useless "DT_RowId" fielld from transformers

--- src/Transformers/Managerarea/ContactTransformer.php
+++ src/Transformers/Managerarea/ContactTransformer.php
@@ -28,7 +28,6 @@ class ContactTransformer extends TransformerAbstract
 
         return $this->escape([
             'id' => (string) $contact->getRouteKey(),
-            'DT_RowId' => 'row_'.$contact->getRouteKey(),
             'given_name' => (string) $contact->given_name,
             'family_name' => (string) $contact->family_name,
             'email' => (string) $contact->email,
