ddf529f3cb1033e8634ed12913ec4f7108d8871e
Abdelrahman Omran <me@omranic.com>
2020-08-03 12:03:12 +0400

    Activate module after installation

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -34,5 +34,7 @@ class InstallCommand extends Command
         $this->call('cortex:publish:contacts', ['--force' => $this->option('force'), '--resource' => $this->option('resource')]);
         $this->call('cortex:migrate:contacts', ['--force' => $this->option('force')]);
         $this->call('cortex:seed:contacts');
+
+        $this->call('cortex:activate', ['--module' => 'cortex/contacts']);
     }
 }
