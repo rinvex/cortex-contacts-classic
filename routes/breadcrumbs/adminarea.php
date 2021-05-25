06f950d123881d984b3b5aaa3e5ec99243bb993c
Abdelrahman Omran <me@omranic.com>
2021-05-25 17:56:58 +0400

    Replace deprecated `Breadcrumbs::register` with `Breadcrumbs::for`

--- routes/breadcrumbs/adminarea.php
+++ routes/breadcrumbs/adminarea.php
@@ -6,33 +6,33 @@ use Cortex\Contacts\Models\Contact;
 use Diglactic\Breadcrumbs\Generator;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 
-Breadcrumbs::register('adminarea.cortex.contacts.contacts.index', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.contacts.contacts.index', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.config('app.name'), route('adminarea.home'));
     $breadcrumbs->push(trans('cortex/contacts::common.contacts'), route('adminarea.cortex.contacts.contacts.index'));
 });
 
-Breadcrumbs::register('adminarea.cortex.contacts.contacts.import', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.contacts.contacts.import', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.contacts.contacts.index');
     $breadcrumbs->push(trans('cortex/contacts::common.import'), route('adminarea.cortex.contacts.contacts.import'));
 });
 
-Breadcrumbs::register('adminarea.cortex.contacts.contacts.import.logs', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.contacts.contacts.import.logs', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.contacts.contacts.index');
     $breadcrumbs->push(trans('cortex/contacts::common.import'), route('adminarea.cortex.contacts.contacts.import'));
     $breadcrumbs->push(trans('cortex/contacts::common.logs'), route('adminarea.cortex.contacts.contacts.import.logs'));
 });
 
-Breadcrumbs::register('adminarea.cortex.contacts.contacts.create', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.contacts.contacts.create', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.contacts.contacts.index');
     $breadcrumbs->push(trans('cortex/contacts::common.create_contact'), route('adminarea.cortex.contacts.contacts.create'));
 });
 
-Breadcrumbs::register('adminarea.cortex.contacts.contacts.edit', function (Generator $breadcrumbs, Contact $contact) {
+Breadcrumbs::for('adminarea.cortex.contacts.contacts.edit', function (Generator $breadcrumbs, Contact $contact) {
     $breadcrumbs->parent('adminarea.cortex.contacts.contacts.index');
     $breadcrumbs->push(strip_tags($contact->full_name), route('adminarea.cortex.contacts.contacts.edit', ['contact' => $contact]));
 });
 
-Breadcrumbs::register('adminarea.cortex.contacts.contacts.logs', function (Generator $breadcrumbs, Contact $contact) {
+Breadcrumbs::for('adminarea.cortex.contacts.contacts.logs', function (Generator $breadcrumbs, Contact $contact) {
     $breadcrumbs->parent('adminarea.cortex.contacts.contacts.index');
     $breadcrumbs->push(strip_tags($contact->full_name), route('adminarea.cortex.contacts.contacts.edit', ['contact' => $contact]));
     $breadcrumbs->push(trans('cortex/contacts::common.logs'), route('adminarea.cortex.contacts.contacts.logs', ['contact' => $contact]));
--- routes/breadcrumbs/managerarea.php
+++ routes/breadcrumbs/managerarea.php
@@ -6,33 +6,33 @@ use Cortex\Contacts\Models\Contact;
 use Diglactic\Breadcrumbs\Generator;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 
-Breadcrumbs::register('managerarea.cortex.contacts.contacts.index', function (Generator $breadcrumbs) {
+Breadcrumbs::for('managerarea.cortex.contacts.contacts.index', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.app('request.tenant')->name, route('managerarea.home'));
     $breadcrumbs->push(trans('cortex/contacts::common.contacts'), route('managerarea.cortex.contacts.contacts.index'));
 });
 
-Breadcrumbs::register('managerarea.cortex.contacts.contacts.import', function (Generator $breadcrumbs) {
+Breadcrumbs::for('managerarea.cortex.contacts.contacts.import', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('managerarea.cortex.contacts.contacts.index');
     $breadcrumbs->push(trans('cortex/contacts::common.import'), route('managerarea.cortex.contacts.contacts.import'));
 });
 
-Breadcrumbs::register('managerarea.cortex.contacts.contacts.import.logs', function (Generator $breadcrumbs) {
+Breadcrumbs::for('managerarea.cortex.contacts.contacts.import.logs', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('managerarea.cortex.contacts.contacts.index');
     $breadcrumbs->push(trans('cortex/contacts::common.import'), route('managerarea.cortex.contacts.contacts.import'));
     $breadcrumbs->push(trans('cortex/contacts::common.logs'), route('managerarea.cortex.contacts.contacts.import.logs'));
 });
 
-Breadcrumbs::register('managerarea.cortex.contacts.contacts.create', function (Generator $breadcrumbs) {
+Breadcrumbs::for('managerarea.cortex.contacts.contacts.create', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('managerarea.cortex.contacts.contacts.index');
     $breadcrumbs->push(trans('cortex/contacts::common.create_contact'), route('managerarea.cortex.contacts.contacts.create'));
 });
 
-Breadcrumbs::register('managerarea.cortex.contacts.contacts.edit', function (Generator $breadcrumbs, Contact $contact) {
+Breadcrumbs::for('managerarea.cortex.contacts.contacts.edit', function (Generator $breadcrumbs, Contact $contact) {
     $breadcrumbs->parent('managerarea.cortex.contacts.contacts.index');
     $breadcrumbs->push(strip_tags($contact->full_name), route('managerarea.cortex.contacts.contacts.edit', ['contact' => $contact]));
 });
 
-Breadcrumbs::register('managerarea.cortex.contacts.contacts.logs', function (Generator $breadcrumbs, Contact $contact) {
+Breadcrumbs::for('managerarea.cortex.contacts.contacts.logs', function (Generator $breadcrumbs, Contact $contact) {
     $breadcrumbs->parent('managerarea.cortex.contacts.contacts.index');
     $breadcrumbs->push(strip_tags($contact->full_name), route('managerarea.cortex.contacts.contacts.edit', ['contact' => $contact]));
     $breadcrumbs->push(trans('cortex/contacts::common.logs'), route('managerarea.cortex.contacts.contacts.logs', ['contact' => $contact]));
